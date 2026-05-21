53966bb8cfb80ff41a9e41c7d68e2d009f66cdb3
euller <eullerdias515@gmail.com>
2026-05-21 14:47:26 -0300

    implementando rotas manuais / /sobre /contato

--- sever.js
+++ sever.js
@@ -1,1 +1,25 @@
-console.log("servidor iniciado")
+const http = require('http');
+
+const servidor = http.createServer((req, res) => {
+  
+  if (req.url === '/') {
+    res.end('Pagina Inicial');
+  }
+  
+  else if (req.url === '/sobre') {
+    res.end('Pagina sobre');
+  }
+  
+  else if (req.url === '/contato') {
+    res.end('Pagina de contato');
+  }
+  
+  else {
+    res.end('Pagina nao encontrada');
+  }
+  
+});
+
+servidor.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
