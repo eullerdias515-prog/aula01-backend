1b45b5d33383373c2872f222e6ef540070770c23
euller <eullerdias515@gmail.com>
2026-05-22 16:16:05 -0300

    Pratica 6: Rotas amigaveis e links atualizados

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/cadastro', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'cadastro.html'));
+});
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
